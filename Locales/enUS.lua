54c60a09fc6bee23ec2555db5f574ee42d5c4b1e
HBLyx_Home <tomli980411@gmail.com>
2026-02-15 13:44:29 -0800

    add more input instructions

--- Locales/enUS.lua
+++ Locales/enUS.lua
@@ -96,10 +96,10 @@ L["InterruptibleColor"] = "Interruptible Color"
 L["NotInterruptibleColor"] = "NotInterruptible Color"
 
 -- MARK: Input
-L["InvalidInput"] = "Invalid Input, please check your input format and value type"
-L["InvalidSpellID"] = "Invalid Spell ID. The Spell ID must be a positive integer."
-L["SpellIDNotFound"] = "Spell ID not found in game. Please be cautious when you add it."
-L["InvalidTime"] = "Invalid Time, it must be a non-negative number in seconds."
+L["InvalidInput"] = "Invalid input, please check all required inputs and their format and type."
+L["InvalidSpellID"] = "Invalid spell ID, the spell ID must be a positive integer and must exist in the game."
+L["SpellIDNotFound"] = "Spell ID not found in the game."
+L["InvalidTime"] = "Invalid time, time inputs must be a non-negative number(float/decimal is allowed) in seconds."
 
 -- MARK: Constants
 L["PetFamily"] = {
@@ -217,5 +217,6 @@ L["AuraSettings"] = "Auras Settings"
 L["AuraSettingsDesc"] = "Although the function is limited, but it still can be supportive. For example, you can track your potion and active trinkets\n\n" ..
 "Taking 12.0 potion as an example, you can add \"Light's Potential\" with its spell ID(1236616) + duration(30s) + cooldown(300s) + expire sound(X sound effect), and then you will get a 30s icon to show aura when you use the potion and a sound alert played after the cooldown time(300s).\n\n" ..
 "You can also use it as a simple sound alert after a period of time after you cast a certain spell. For example, after 300s of using \"health potion\" play a sound effect, you can set spell ID=\"spell ID of health potion\" + duration=0 + cooldown=300 + expire sound\n\n" ..
-"|cffff0000NOTE|r: Spell IDs for items are \"spell ID\" instead of \"item ID\"(you can aquire them with other addon like \"idTip\"), and you can delete/update(delete/add button below) an aura by entering spell ID manually or selecting an existing aura by the dropdown menu below. Additionally, duration and cooldown are in seconds. Eventually, if you want to cancel a sound effect, you can select \"None\" for the sound effect to cancel it."
+"|cffff0000NOTE|r: Spell IDs for items are \"spell ID\" instead of \"item ID\"(you can aquire them with other addon like \"idTip\"), and you can delete/update(delete/add button below) an aura by entering spell ID manually or selecting an existing aura by the dropdown menu below. Additionally, duration and cooldown are in seconds. Eventually, if you want to cancel a sound effect, you can select \"None\" for the sound effect to cancel it.\n\n" ..
+"|cffffff00INPUTS|r: Spell ID, Duration, and Cooldown are required to add. Spell ID must be a positive integer and exist in the game, Duration and Cooldown must be non-negative numbers(can be decimal/float or zero)."
 L["SelectAura"] = "Select an existing aura"

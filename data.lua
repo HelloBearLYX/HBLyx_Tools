032ab20968ee5d71045d679076b166a55c48431a
Yaoxin Li (Tom) <58971826+HelloBearLYX@users.noreply.github.com>
2026-07-02 16:47:28 -0700

    data update and correlated update

--- data.lua
+++ data.lua
@@ -3,89 +3,140 @@ local L = LibStub("AceLocale-3.0"):GetLocale(ADDON_NAME)
 addon.data = {}
 
 addon.data.SEASON_MAP = {
-	-- MARK: current season 12.0
-    [402] = {
-		portalID = 393273,
-		name = L["Algeth'ar Academy"],
-		short = L["Algeth'ar Academy_short"],
-	},
-	[239] = {
-		portalID = 1254551,
-		name = L["Seat of the Triumvirate"],
-		short = L["Seat of the Triumvirate_short"],
-	},
-	[559] = {
-		portalID = 1254563,
-		name = L["Nexus-Point Xenas"],
-		short = L["Nexus-Point Xenas_short"],
-	},
-	[560] = {
-		portalID = 1254559,
-		name = L["Maisara Caverns"],
-		short = L["Maisara Caverns_short"],
-	},
-	[161] = {
-		portalID = {1254557, 159898},
-		name = L["Skyreach"],
-		short = L["Skyreach_short"],
-	},
-	[557] = {
-		portalID = 1254400,
-		name = L["Windrunner Spire"],
-		short = L["Windrunner Spire_short"],
-	},
-	[558] = {
-		portalID = 1254572,
-		name = L["Magister's Terrace"],
-		short = L["Magister's Terrace_short"],
-	},
-	[556] = {
-		portalID = 1254555,
-		name = L["Pit of Saron"],
-		short = L["Pit of Saron_short"],
-	},
-
-	-- MARK: 12.1
-	-- TODO: Update portalID
-	[249] = {
-		portalID = 134400,
-		name = select(1, EJ_GetInstanceInfo(1041)) or "King's Rest",
-		short = L["King's Rest_short"],
-	},
-	[250] = {
-		portalID = 134401,
-		name = select(1, EJ_GetInstanceInfo(1030)) or "Temple of Sethraliss",
-		short = L["Temple of Sethraliss_short"],
-	},
-	[399] = {
-		portalID = 393256,
-		name = select(1, EJ_GetInstanceInfo(1202)) or "Ruby Life Pools",
-		short = L["Ruby Life Pools_short"],
-	},
-	[584] = {
-		portalID = 134401,
-		name = select(1, EJ_GetInstanceInfo(1309)) or "The Blinding Vale",
-		short = L["The Blinding Vale_short"],
-	},
-	[585] = {
-		portalID = 134401,
-		name = select(1, EJ_GetInstanceInfo(1313)) or "Voidscar Arena",
-		short = L["Voidscar Arena_short"],
-	},
-	[586] = {
-		portalID = 134401,
-		name = select(1, EJ_GetInstanceInfo(1311)) or "Den of Nalorakk",
-		short = L["Den of Nalorakk_short"],
+	-- TODO: Update unknown portalIDs (placeholder 134401): 587, 586, 585, 584, 250
+	-- Midnight 12.0
+	[588] = {
+		enabled = true,
+		instanceID = 2993,
+		journalInstanceID = 1322,
+		portalID = 1286812,
+		name = select(1, EJ_GetInstanceInfo(1322)) or "Altar of Fangs",
+		short = L["Altar of Fangs_short"],
 	},
 	[587] = {
+		enabled = true,
+		instanceID = 2813,
+		journalInstanceID = 1304,
 		portalID = 134401,
 		name = select(1, EJ_GetInstanceInfo(1304)) or "Murder Row",
 		short = L["Murder Row_short"],
 	},
-	[588] = {
+	[586] = {
+		enabled = true,
+		instanceID = 2825,
+		journalInstanceID = 1311,
 		portalID = 134401,
-		name = select(1, EJ_GetInstanceInfo(1322)) or "Altar of Fangs",
-		short = L["Altar of Fangs_short"],
+		name = select(1, EJ_GetInstanceInfo(1311)) or "Den of Nalorakk",
+		short = L["Den of Nalorakk_short"],
+	},
+	[585] = {
+		enabled = true,
+		instanceID = 2923,
+		journalInstanceID = 1313,
+		portalID = 134401,
+		name = select(1, EJ_GetInstanceInfo(1313)) or "Voidscar Arena",
+		short = L["Voidscar Arena_short"],
+	},
+	[584] = {
+		enabled = true,
+		instanceID = 2859,
+		journalInstanceID = 1309,
+		portalID = 134401,
+		name = select(1, EJ_GetInstanceInfo(1309)) or "The Blinding Vale",
+		short = L["The Blinding Vale_short"],
+	},
+	[560] = {
+		enabled = false,
+		instanceID = 2874,
+		journalInstanceID = 1315,
+		portalID = 1254559,
+		name = select(1, EJ_GetInstanceInfo(1315)) or "Maisara Caverns",
+		short = L["Maisara Caverns_short"],
+	},
+	[559] = {
+		enabled = false,
+		instanceID = 2915,
+		journalInstanceID = 1316,
+		portalID = 1254563,
+		name = select(1, EJ_GetInstanceInfo(1316)) or "Nexus-Point Xenas",
+		short = L["Nexus-Point Xenas_short"],
+	},
+	[558] = {
+		enabled = false,
+		instanceID = 2811,
+		journalInstanceID = 1300,
+		portalID = 1254572,
+		name = select(1, EJ_GetInstanceInfo(1300)) or "Magister's Terrace",
+		short = L["Magister's Terrace_short"],
+	},
+	[557] = {
+		enabled = false,
+		instanceID = 2805,
+		journalInstanceID = 1299,
+		portalID = 1254400,
+		name = select(1, EJ_GetInstanceInfo(1299)) or "Windrunner Spire",
+		short = L["Windrunner Spire_short"],
+	},
+	-- DF
+    [402] = {
+		enabled = false,
+		instanceID = 2526,
+		journalInstanceID = 1201,
+		portalID = 393273,
+		name = select(1, EJ_GetInstanceInfo(1201)) or "Algeth'ar Academy",
+		short = L["Algeth'ar Academy_short"],
+	},
+	[399] = {
+		enabled = true,
+		instanceID = 2521,
+		journalInstanceID = 1202,
+		portalID = 393256,
+		name = select(1, EJ_GetInstanceInfo(1202)) or "Ruby Life Pools",
+		short = L["Ruby Life Pools_short"],
+	},
+	-- BFA
+	[250] = {
+		enabled = true,
+		instanceID = 1877,
+		journalInstanceID = 1030,
+		portalID = 134401,
+		name = select(1, EJ_GetInstanceInfo(1030)) or "Temple of Sethraliss",
+		short = L["Temple of Sethraliss_short"],
+	},
+	[249] = {
+		enabled = true,
+		instanceID = 1762,
+		portalID = 134400,
+		journalInstanceID = 1041,
+		name = select(1, EJ_GetInstanceInfo(1041)) or "King's Rest",
+		short = L["King's Rest_short"],
+	},
+	-- Legion
+	[239] = {
+		enabled = false,
+		instanceID = 1753,
+		journalInstanceID = 945,
+		portalID = 1254551,
+		name = select(1, EJ_GetInstanceInfo(945)) or "Seat of the Triumvirate",
+		short = L["Seat of the Triumvirate_short"],
+	},
+	-- WoD
+	[161] = {
+		enabled = false,
+		instanceID = 1209,
+		journalInstanceID = 476,
+		portalID = {1254557, 159898},
+		name = select(1, EJ_GetInstanceInfo(476)) or "Skyreach",
+		short = L["Skyreach_short"],
+	},
+	-- WoLK
+	[556] = {
+		enabled = false,
+		instanceID = 658,
+		journalInstanceID = 278,
+		portalID = 1254555,
+		name = select(1, EJ_GetInstanceInfo(278)) or "Pit of Saron",
+		short = L["Pit of Saron_short"],
 	},
 }
 
